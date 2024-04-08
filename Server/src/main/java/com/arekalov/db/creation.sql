0e526e186d9b7d9b1cc72b6ba4dfefddb1a66863
arekalov <artyom.rekalov@gmail.com>
2024-04-08 12:31:16 +0300

    add creation.sql script

---
+++ Server/src/main/java/com/arekalov/db/creation.sql
@@ -0,0 +1,51 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS LOCATION
+(
+    id   SERIAL PRIMARY KEY,
+    x    REAL NOT NULL,
+    y    REAL NOT NULL,
+    name TEXT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ADRESS
+(
+    id       SERIAL PRIMARY KEY,
+    street   TEXT                    NULL,
+    location INT REFERENCES LOCATION NOT NULL
+);
+
+CREATE TYPE OrganizationType AS ENUM ('COMMERCIAL', 'TRUST',
+    'PRIVATE_LIMITED_COMPANY', 'OPEN_JOINT_STOCK_COMPANY');
+
+CREATE TABLE IF NOT EXISTS ORGANIZATION
+(
+    id             SERIAL PRIMARY KEY,
+    name           TEXT                  NOT NULL,
+    annualTurnover REAL,
+    type           OrganizationType      NOT NULL,
+    postalAdress   INT REFERENCES ADRESS NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS COORDINATE
+(
+    x REAL NOT NULL,
+    y REAL NOT NULL,
+    PRIMARY KEY (x, y)
+);
+
+CREATE TYPE UnitOfMeasure AS ENUM ('KILOGRAMS', 'SQUARE_METERS', 'PCS', 'MILLIGRAMS');
+
+CREATE TABLE IF NOT EXISTS PRODUCT
+(
+    id              SERIAL PRIMARY KEY,
+    name            TEXT                        NOT NULL,
+    coordinates     INT REFERENCES COORDINATE   NOT NULL,
+    localDate       TIMESTAMP                   NOT NULL,
+    price           BIGINT                      NULL,
+    partNumber      TEXT                        NOT NULL,
+    manufactureCost INT                         NOT NULL,
+    unitOfMeasure   UnitOfMeasure               NOT NULL,
+    manufacturer     INT REFERENCES ORGANIZATION NOT NULL
+
+);
+END TRANSACTION;
